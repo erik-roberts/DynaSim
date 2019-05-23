7d5acc328ce0d74ee00a74fe1ccafea913e7ff4b
Erik Roberts <eriktheblue@gmail.com>
2019-05-23 11:27:52 -0400

    add tspan

--- demos/devDemos/dsDemos_covar_temp.m
+++ demos/devDemos/dsDemos_covar_temp.m
@@ -73,6 +73,8 @@ s.connections(2).parameters={'tauD',2,'gSYN',.1,'netcon',ones(80,20)};
 %       This sets modifications:
 %           * E_EK1 = -75, E_EK2 = -80, I_EK1 = -85, I_EK2 = -90.
 %           * E_EK1 = -55, E_EK2 = -60, I_EK1 = -65, I_EK2 = -70.
-dsSimulate(s,'save_data_flag',0,'study_dir','demo_sPING_covary',...
-                'vary',vary,'verbose_flag',1);
+data = dsSimulate(s,'save_data_flag',0,'study_dir','demo_sPING_covary',...
+                'vary',vary,'verbose_flag',1, 'tspan',[0 100]);
+dsPlot(data)
+              
 
